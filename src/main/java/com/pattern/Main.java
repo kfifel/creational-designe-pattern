3bc0cb48870a07776ed644a186c5f251482d187a
khalid fifel <fifle.khalid99@gmail.com>
2023-10-11 20:34:44 +0100

    adding the Singleton pattern

--- src/main/java/com/pattern/Main.java
+++ src/main/java/com/pattern/Main.java
@@ -1,15 +1,11 @@
 package com.pattern;
 
-import com.pattern.Singleton.DataBaseSingleton;
 import com.pattern.builder.User;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) {
+
+
+    // use the builder pattern to create a user object
         User user =
             new User
                 .UserBuilder()
